ba652abf8d8322b99f9e8eee006ad5b806d25986
Данилов Алексей Николаевич <ANDanilov@alfabank.ru>
2025-09-11 23:56:48 +1200

    Initial commit

---
+++ GithubRepoExplorer/GithubRepoExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubRepoExplorerApp.swift
+//  GithubRepoExplorer
+//
+//  Created by Aleksei Danilov on 11.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct GithubRepoExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
